443a5412cfb5cd1a111026b5037f15d600cbb343
akber <akberakhmedzadeh@gmail.com>
2021-05-05 01:33:54 +0400

    bugfix

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   $(window).on("scroll", function () {
     let top = $(this).scrollTop();
 
-    if (top > 100) {
+    if (top > 300) {
       $(".noHeight").addClass("fixedActive");
       $('.webHeader .header__toolsBox').hide()
     } else {
